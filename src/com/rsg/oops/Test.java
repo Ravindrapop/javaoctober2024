276ea70d0c123ceeb9886a8f7e2c6cf26b6b60c0
Ravindra Singh <ravindra12329@gmail.com>
2024-12-11 08:32:00 +0530

    examples added for loading case 1, case 2

---
+++ src/com/rsg/oops/Test.java
@@ -0,0 +1,29 @@
+package com.rsg.oops;
+
+public class Test {
+	
+	public void m1(Object o) {
+		System.out.println("Object version");
+	}
+	
+	
+	public void m1(String s) {
+		System.out.println("String version");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		Test test = new Test();
+		
+		test.m1(new Object());  // Object version
+		
+		test.m1("Durga");  // String version
+		
+		test.m1(null); // String version
+
+
+	}
+
+}
